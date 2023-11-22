5f60857845bf4b991bbd05bbc345af03623decbd
Gaurav <gauravlavand0102@gmail.com>
2023-11-22 11:56:25 +0530

    order project setup

---
+++ OrderDemoApp/src/main/java/com/order/entity/Order.java
@@ -0,0 +1,32 @@
+package com.order.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String productName;
+	
+	private long price;
+	
+	private int quantity;
+	
+	@OneToOne
+	private User user;
+}
